b65bd2d9552b3ff4b616ce50c2e4e055118508fa
zubiii <zubair.javed.ch@gmail.com>
2023-05-30 07:23:28 +0500

    🚀 Add @nestjs/config package in order to handel env file

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'mysql://root:example@0.0.0.0:3306/nestjs_api?schema=public',
+          /**
+           * There are two ways to access env file now.
+           *  1. process.env.DATABASE_URL
+           *  2. By using ConfigService from '@nestjs/config'
+           * Let's try second way
+           */
+          url: config.get('DATABASE_URL'),
         },
       },
     });
